77733977e5f39b449d1e7efbc3066fa9af280144
Javier Alvarez <jalvarezsamayoa@gmail.com>
2012-12-14 11:34:32 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminOpenwolf::Application.config.secret_token = '8e567ce42be672f9b20ede92a80b717807b47621fb7117d242c8354c96c7bd97f76e89b22b6f7f0e4b5ac6bbafe82393d389dec133d59fcdf65152d95ee04d32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdminOpenwolf::Application.config.session_store :cookie_store, key: '_AdminOpenwolf_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdminOpenwolf::Application.config.session_store :active_record_store
